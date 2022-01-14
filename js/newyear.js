c4d8941d19f95e39484ca043c4e859acc89da03c
Miekastang <63377374+Miekastang@users.noreply.github.com>
2022-01-13 19:27:41 -0500

    Update newyear.js

--- js/newyear.js
+++ js/newyear.js
@@ -7,251 +7,236 @@
 function getFortune (str){
 var value = Math.floor(Math.random() * 81);;
   if (value == 0){
-    return "You will go see birds.";
+    return "You will buy a new gold water bottle.";
   }
   else if (value == 1){
-    return "You are going to the moon and Mars!";
+    return "You will have a great day and you will get some cookies and cake and love and hearts and hearts and hearts!";
   }
   else if (value == 2){
-    return "You will go to bike school.";
+    return "You will write your own song and you will sing it to the world.";
   }
   else if (value == 3){
-    return "You will get a Minnie Mouse stuffy.";
+    return "You will explore cheetahs and see baby cheetahs.";
   }
   else if (value == 4){
-    return "You will get a pet parrot.";
+    return "You will see a mermaid.";
   }
   else if (value == 5){
-    return "You will read 100 books!";
+    return "You will see Princess Daisy.";
   }
    else if (value == 6){
-      return "You will get a new bike.";
+      return "You will go to Hawaii and see people and water slides. There’s also an ocean.";
     }
     else if (value == 7){
-      return "You will go see wild tigers.";
+      return "You will get more LEGO sets.";
     }
     else if (value == 8){
-      return "You will go in a really fast Porsche.";
+      return "You will get toys and money.";
     }
     else if (value == 9){
-      return "You will buy 100 bikes.";
+      return "You will go to Colorado and see a big pool.";
     }
     else if (value == 10){
-      return "You will go to Hogwarts.";
+      return "You will see so many unicorns in Unicorn Land!";
     }
     else if (value == 11){
-      return "You will have a rocket ship and go to the moon!";
+      return "You will see lots of Minions.";
     }
     else if (value == 12){
-      return "You will grow a flower that smells good.";
+      return "You will go to Tahoe.";
     }
     else if (value == 13){
-      return "You will have a new watering can.";
+      return "You will go to the beach and find shells.";
     }
     else if (value == 14){
-      return "You will make something awesome with your Grandma.";
-    }
-    else if (value == 15){
-      return "You will have a new job.";
-    }
-    else if (value == 16){
-      return "You will have an oak tree.";
-    }
-    else if (value == 17){
-      return "You will have a baby chick.";
-    }
-    else if (value == 18){
-      return "You will see wild sharks!";
-    }
-    else if (value == 19){
-      return "You will be able to see the future.";
-    }
-    else if (value == 20){
-      return "You will see pangolins.";
-    }
-    else if (value == 21){
-      return "You will see a tree and a bird eating bird food.";
-    }
-    else if (value == 22){
-      return "You will see something that’s funny.";
-    }
-    else if (value == 23){
-      return "You will see a silly face.";
-    }
-    else if (value == 24){
-      return "You will have special magic powers.";
-    }
-    else if (value == 25){
-      return "You will make ice swords.";
-    }
-    else if (value == 26){
-      return "You will have a picture of Pooh Bear.";
-    }
-    else if (value == 27){
-      return "You will make a chocolate cake.";
-    }
-    else if (value == 28){
-      return "You will see a bike driving itself.";
-    }
-    else if (value == 29){
-      return "You will learn how to speak dog.";
-    }
-    else if (value == 30){
-      return "You will have a wonderful amazing birthday!";
-    }
-    else if (value == 31){
-      return "You will get a Mickey Mouse stuffy.";
-    }
-    else if (value == 32){
-      return "You will have a Ryan toy.";
-    }
-    else if (value == 33){
-      return "You will get an Elsa stuffy.";
-    }
-    else if (value == 34){
-      return "You will see a butterfly.";
-    }
-    else if (value == 35){
-      return "You will get a black stallion.";
-    }
-    else if (value == 36){
-      return "You will get a Transformer toy.";
-    }
-    else if (value == 37){
-      return "You will be freezing and then melt to pieces.";
-    }
-    else if (value == 38){
-      return "You will see a rainbow.";
-    }
-    else if (value == 39){
-      return "You will have a fish tank.";
-    }
-    else if (value == 40){
-      return "You will have Calico Critters.";
-    }
-    else if (value == 41){
-      return "You will have a rainbow shine in your eye.";
-    }
-    else if (value == 42){
-      return "You will get an Optimus and a Bumblebee which combine into Bumbletron!";
-    }
-    else if (value == 43){
-      return "You will see a rainbow star!";
-    }
-    else if (value == 44){
-      return "You will make beautiful snowflakes because we’re in winter.";
-    }
-    else if (value == 45){
-      return "You will go see unicorns at the zoo.";
-    }
-    else if (value == 46){
-      return "Your whole house will be filled up with stuffies!";
-    }
-    else if (value == 47){
-      return "You will knock on a honey hive and a bee will be your friend instead of chase you and sting you.";
-    }
-    else if (value == 48){
-      return "You will learn to speak mouse.";
-    }
-    else if (value == 49){
       return "You will plant a tree.";
     }
+    else if (value == 15){
+      return "You will find a map at the store and it will take you to an elevator. The elevator will take you down a deep hole and you will find a mummy that is nice.";
+    }
+    else if (value == 16){
+      return "You will get new rain boots.";
+    }
+    else if (value == 17){
+      return "You will go to the jungle and see a tiger.";
+    }
+    else if (value == 18){
+      return "You will go to Paris with your family and see the King and the Queen.";
+    }
+    else if (value == 19){
+      return "You will go to Disneyland and meet Disney Princesses.";
+    }
+    else if (value == 20){
+      return "You will go to Disneyland in Paris.";
+    }
+    else if (value == 21){
+      return "You will go to the zoo and see little bears.";
+    }
+    else if (value == 22){
+      return "You will have an allowance and buy more toys.";
+    }
+    else if (value == 23){
+      return "You will make friends with a unicorn.";
+    }
+    else if (value == 24){
+      return "You will find a nice monster and then you dance with it.";
+    }
+    else if (value == 25){
+      return "You will get a cookie with a fortune in it.";
+    }
+    else if (value == 26){
+      return "You will get toy cars and stuff.";
+    }
+    else if (value == 27){
+      return "You will find dolphins and swim and jump with them.";
+    }
+    else if (value == 28){
+      return "You will swim with a sea turtle and have fun with them.";
+    }
+    else if (value == 29){
+      return "You will love your mom and dad.";
+    }
+    else if (value == 30){
+      return "You will find a gift.";
+    }
+    else if (value == 31){
+      return "Everything wonderful from the world will come to you.";
+    }
+    else if (value == 32){
+      return "You will have gifts in the new year.";
+    }
+    else if (value == 33){
+      return "You will live a wonderful life in the world.";
+    }
+    else if (value == 34){
+      return "I hope you buy new pants.";
+    }
+    else if (value == 35){
+      return "You will have too much love (I love you too much!).";
+    }
+    else if (value == 36){
+      return "Someone will give you flowers.";
+    }
+    else if (value == 37){
+      return "You will buy flowers.";
+    }
+    else if (value == 38){
+      return "You will get a Santa doll with presents.";
+    }
+    else if (value == 39){
+      return "You will love your brother.";
+    }
+    else if (value == 40){
+      return "Your birthday will come up soon.";
+    }
+    else if (value == 41){
+      return "You will hang out with friends.";
+    }
+    else if (value == 42){
+      return "Your lucky number is 5013.";
+    }
+    else if (value == 43){
+      return "You will ride a unicorn on your birthday and fly on it.";
+    }
+    else if (value == 44){
+      return "You will buy new underwear.";
+    }
+    else if (value == 45){
+      return "You will make new friends.";
+    }
+    else if (value == 46){
+      return "You will buy new books.";
+    }
+    else if (value == 47){
+      return "You will get a new cat that doesn’t poop in the cat box as soon as you clean it.";
+    }
+    else if (value == 48){
+      return "You will love yourself.";
+    }
+    else if (value == 49){
+      return "You will see golden eggs.";
+    }
     else if (value == 50){
-      return "You will swim in sea weed.";
+      return "You will find snow and play in it.";
     }
     else if (value == 51){
-      return "You will like your family.";
+      return "You will see 100 rainbows.";
     }
     else if (value == 52){
-      return "You will love your family.";
+      return "Your lucky letters will be LOL.";
     }
     else if (value == 53){
-      return "";
+      return "You will have friends standing beside you every day.";
     }
     else if (value == 54){
-      return "You will go on a boat, fishing";
+      return "You will have the best day ever on your birthday.";
     }
     else if (value == 55){
-      return "You will have a happy new year and NO CORONA!";
+      return "You will love your best friend.";
     }
     else if (value == 56){
-      return "You will have super powers.";
+      return "You will go to 100 parks.";
     }
     else if (value == 57){
-      return "You will get 20 presents from Santa because you’ve been really good.";
+      return "You will slide down a pole (and up too!)";
     }
     else if (value == 58){
-      return "You will see lots of animals.";
+      return "You will go in an airplane and see ice cream falling from the sky.";
     }
     else if (value == 59){
-      return "You will see lots of ocean animals.";
+      return "You will get more candy at Halloween.";
     }
     else if (value == 60){
-      return "You will see turtles, octopuses and squids.";
+      return "You will get a giant gingerbread house from the store.";
     }
     else if (value == 61){
-      return "You will see stars in the night.";
+      return "Your room will be filled with stuffies.";
     }
     else if (value == 62){
-      return "You will see the moon in the night.";
+      return "A dragon will be friends with you.";
     }
     else if (value == 63){
-      return "You will have a good time and you will love everybody.";
+      return "You will get a playground at the back of your house.";
     }
     else if (value == 64){
-      return "You will swim in the sea.";
+      return "On your birthday you will get cupcakes and cake and so many friends to play with you.";
     }
     else if (value == 65){
-      return "You will have a fish tank.";
+      return "You will play with yarn.";
     }
     else if (value == 66){
-      return "You will go ice skating.";
+      return "You will be able to talk to animals.";
     }
     else if (value == 67){
-      return "You will see a mirage.";
+      return "You will be happy being a king or queen.";
     }
     else if (value == 68){
-      return "You will have a job.";
+      return "You will get a pet dog.";
     }
     else if (value == 69){
-      return "You will have a new cat.";
+      return "You will find a rainbow or a unicorn.";
     }
     else if (value == 70){
-      return "You will grow a flower.";
+      return "You will get a cat and play with it.";
     }
     else if (value == 71){
-      return "You will have a great birthday.";
+      return "You will blast off into space with my brother.";
     }
     else if (value == 72){
-      return "You will have NO CORONA.";
+      return "You will love your parents so much!";
     }
     else if (value == 73){
-      return "You will see a pink dolphin.";
+      return "You will love your baby sister.";
     }
     else if (value == 74){
-      return "You will learn lots of things.";
+      return "You will find a map and it will take you to the secret jungle.";
     }
     else if (value == 75){
-      return "You will see tons of cats.";
+      return "You will have a pet frog.";
     }
     else if (value == 76){
-      return "You will see lots of fishies.";
+      return "You will learn to speak mouse.";
     }
-    else if (value == 77){
-      return "You will learn lots of letters.";
-    }
-    else if (value == 78){
-      return "You will see lots of dogs and the dogs will lick you.";
-    }
-    else if (value == 79){
-      return "You will see a flower grow.";
-    }
-    else if (value == 80){
-      return "You will be with a family and play with a puppy.";
-    }
-  else {
-    return "You will have a pet owl.";
-  }
 }
 
 //On button run function
