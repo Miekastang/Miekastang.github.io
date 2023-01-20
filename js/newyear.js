fe729c76e9c4bda8ea5f1cc12df4f253999f1ce3
Miekastang <63377374+Miekastang@users.noreply.github.com>
2023-01-20 09:39:07 -0500

    Update newyear.js

--- js/newyear.js
+++ js/newyear.js
@@ -5,7 +5,7 @@
 //function get fortune with math.random
 
 function getFortune (str){
-var value = Math.floor(Math.random() * 76);;
+var value = Math.floor(Math.random() * 47);;
   if (value == 0){
     return "You will see somebody that you can love.";
   }
